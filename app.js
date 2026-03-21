9feff62f8e4e06300a55011bdf4f09beb9292429
David MARTINS <david.joaquim.martins@gmail.com>
2026-03-21 15:09:29 +0100

    aggregator console

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const app = express();
 const APPS = [
   'https://lddm-list-2.onrender.com/wake',
   'https://a-fazer.onrender.com/wake',
-
   'https://labonnepaye-3.onrender.com/wake',
 
   'https://morningping.onrender.com/wake',
   'https://skill-up-n3xe.onrender.com'
 ];
 
-app.get('/wake', async (req, res) => {
+  app.get('/wake', async (req, res) => {
   const results = await Promise.allSettled(
-    APPS.map(url => fetch(url))
-    console.log(`url : ${url} ok : ${res.ok} status : ${res.status}`)
+    APPS.map(async (url) => {
+      const r = await fetch(url);
+      console.log(`url: ${url} | status: ${r.status} | ok: ${r.ok}`);
+      return r;
+    })
   );
   res.status(200).json({ status: 'ok', time: new Date().toISOString() });
 });
+
 
 app.listen(3000);
